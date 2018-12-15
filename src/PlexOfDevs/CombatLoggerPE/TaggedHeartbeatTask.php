415c0b863db5b019ce0e3defcf767f51f29ce91a
PlexOfDevs <45082070+PlexOfDevs@users.noreply.github.com>
2018-12-15 00:28:31 -0500

    Added Files

---
+++ src/PlexOfDevs/CombatLoggerPE/TaggedHeartbeatTask.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * CombatLoggerPE plugin for PocketMine-MP
+ * Copyright (C) 2018 PlexOfDevs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+namespace PlexOfDevs\CombatLoggerPE;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+
+class TaggedHeartbeatTask extends Task {
+
+	public function onRun(int $currentTick) {
+		$plugin = CombatLogger::getInstance();
+		foreach($plugin->taggedPlayers as $name => $time) {
+			$time--;
+			if($time <= 0) {
+				$plugin->setTagged($name, false);
+				$player = $plugin->getServer()->getPlayerExact($name);
+				if($player instanceof Player) $player->sendMessage($plugin->getMessageManager()->getMessage("player-tagged-timeout"));
+				return;
+			}
+			$plugin->taggedPlayers[$name]--;
+		}
+	}
+
+}
